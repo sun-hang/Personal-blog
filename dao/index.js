ef3ebd9f881627267b808c0de98485626dcacbb5
sun <1844528595@qq.com>
2020-07-28 07:18:21 +0800

    mysql-pwass

--- dao/index.js
+++ dao/index.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 const { mysqlLoger } = require('../logger')
-const sequelize = new Sequelize('my_blog', 'root', 'root', {
+const sequelize = new Sequelize('my_blog', 'root', '1234', {
     host: 'localhost',
     dialect: 'mysql',
     logging: (msg) => {
